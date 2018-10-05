31e13e3f5166b98a242ce96a6df47ca0f60163dc
Dasha Utyuzh <dutyuzh@gmail.com>
2018-10-05 09:03:21 -0400

    update context supporting flatten array
    update context supporting flatten array

--- flatarray.js
+++ flatarray.js
@@ -4,3 +4,8 @@ function flatten(arr) {
         return acc.concat( Array.isArray(x) ? flatten(x) : [x] );
     }, []);
 };
+
+let animals = [['kittens','giraffes'],['puppies','sharks']]
+console.log('not flat', animals); //not flat: 0: ['kittens', 'giraffes'] 1: ['puppies','sharks']
+let mergedAnimals = flatten(animals);
+console.log('flat!', mergedAnimals); //flat!: ['kittens', 'giraffes', 'puppies','sharks']
